d57422b1471d8d73b34f434d0356c443473a3162
stingray0225 <114566779+stingraypark@users.noreply.github.com>
2025-05-02 01:19:15 +0900

    2

--- scripts/generator.js
+++ scripts/generator.js
@@ -11,6 +11,38 @@ function generateNickname() {
     const useManualName = true;
     const manualName = "박가온";
     const nameGender = "M";
+    const removeSpace = true;
 
-    
+    // Init
+    let result = ""
+
+    // Add location
+    const location = pickRandom(data.locations);
+    result += `${location.name} `;
+
+    // Add suffix
+    const suffixGroup = data.suffixGroups[location.suffixGroupIndex];
+    for (let i = 0; i < suffixGroup.length; i++) {
+        result += `${pickRandom(suffixGroup[i])} `;
+    }
+
+    // Add manual name
+    if (useName && useManualName) {
+        result += manualName;
+    }
+
+    // Add random name
+    if (useName && !useManualName) {
+        result += pickRandom(data.familyNames);
+    }
+
+    // Space removal
+    if (removeSpace) {
+        result = result.replace(/\s/g, '');
+    }
+
+    // Return
+    return result;
 }
+
+console.log(generateNickname());
