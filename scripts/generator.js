5ae5a9ae3006eca9834937d847351a1737af1268
stingray0225 <114566779+stingraypark@users.noreply.github.com>
2025-05-02 00:44:46 +0900

    init

---
+++ scripts/generator.js
@@ -0,0 +1,16 @@
+// Import data
+import { data } from './data.js';
+
+function pickRandom(arr) {
+    return arr[Math.floor(Math.random() * arr.length)];
+}
+
+function generateNickname() {
+    // OPTIONS - CHANGE!!!
+    const useName = true;
+    const useManualName = true;
+    const manualName = "박가온";
+    const nameGender = "M";
+
+    
+}
